6a7d5cfa20a55d9f5871da5db2816964b1a6bc2a
halimmharis <harishalim@student.ub.ac.id>
2022-07-08 11:53:08 +0700

    kucoba

--- src/Pages/Admin/admin.js
+++ src/Pages/Admin/admin.js
@@ -1,72 +1,17 @@
 import React from "react";
-
+import './Admin.css'
 const Admin = () => {
   return (
-    <div className="grid" style={{ "height": "100vh" }}>
-      <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark col-sm-2" style={{ "height": "100%" }}>
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-          <h1>HALO</h1>
-          <span className="fs-4">Sidebar</span>
-        </a>
-        <hr/>
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Orders
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Customers
-            </a>
-          </li>
-        </ul>
-        <div className="dropdown">
-          <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-            <strong>mdo</strong>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li>
-              <a className="dropdown-item" href="#">
-                New project...
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Profile
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div className="row ">
+      <div className="sidebargw col-sm-2 border border-2 d-flex flex-column bg-dark bg-gradient text-white">
+        <span className="d-flex justify-content-center">JUDUL</span>
+        <span>JUDUL</span>
+        <span>JUDUL</span>
+        <span>JUDUL</span>
+
+      </div>
+      <div className="col-sm-10 border border-2">
+
       </div>
     </div>
   );
