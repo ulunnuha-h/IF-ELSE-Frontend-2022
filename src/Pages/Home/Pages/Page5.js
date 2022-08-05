102d274949a1586dce5cbf968b53d09dc52d6376
eryc_s.z.w <erycszw10@gmail.com>
2022-08-05 18:16:31 +0700

    [Erycson] fix code

--- src/Pages/Home/Pages/Page5.js
+++ src/Pages/Home/Pages/Page5.js
@@ -10,7 +10,7 @@ const Page5 = () => {
         <>
         <div>
             <div className="w-100 text-light position-absolute p-1 p-lg-5" style={{zIndex:'1',height:'90vh',overflow:'hidden'}}>
-                <div className="h-200 d-flex flex-column align-items-center" style={{borderRadius:'24px',overflow:'hidden'}}>
+                <div className="h-200 d-flex flex-column align-items-center" style={{clborderRadius:'24px',overflow:'hidden'}}>
                     <h2 className="judul">RANGKAIAN IF ELSE 2022</h2>
                     <Rangkaians/>
                 </div>
