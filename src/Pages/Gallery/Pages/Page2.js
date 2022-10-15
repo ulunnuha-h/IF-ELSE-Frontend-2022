3e6eca538e5f3ae8f307f6ab7c75b7c7ee96a0ef
Fajri <fajri.harlan.fh@gmail.com>
2022-10-15 18:56:27 +0700

    [Fajri] fixing page 2 gallery

--- src/Pages/Gallery/Pages/Page2.js
+++ src/Pages/Gallery/Pages/Page2.js
@@ -6,12 +6,14 @@ import Rangkaian1 from "../Components/Rangkaian1";
 const Page2 = () => {
 
     return(
-        <div className="px-2 pt-5 w-100 d-flex align-items-end" style={{background:'linear-gradient(#451400,#F58000)',maxHeight:'90vh'}} >
-            <Bg className="position-absolute" style={{left:'0'}}/>
-            <main className="container-lg">
-                <Rangkaian1/>
-            </main>
-        </div>
+        <>
+            <div className="px-2 pt-5 w-100 d-flex align-items-end" style={{background:'linear-gradient(#451400,#F58000)',maxHeight:'9000vh'}} >
+                <Bg className="position-absolute" style={{left:'0', zIndex:'1'}}/>
+                <main className="container-lg" style={{zIndex:'10', marginTop:'15vh'}} >
+                    <Rangkaian1/>
+                </main>
+            </div>
+        </>
     );
 }
 
