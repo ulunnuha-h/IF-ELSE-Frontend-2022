59e4b603118d8a5a93cd48e3bfecf1eec2ebecb8
Fajri <fajri.harlan.fh@gmail.com>
2022-10-19 18:23:18 +0700

    [Fajri] add Coming Soon Page

--- src/Pages/Gallery/Pages/Page2.js
+++ src/Pages/Gallery/Pages/Page2.js
@@ -1,17 +1,26 @@
 import React from "react";
-import './Page1.css';
+import './Page2.css';
 import {ReactComponent as Bg} from '../../../Assets/Page2/Desert2.svg'
 import Rangkaian1 from "../Components/Rangkaian1";
+import Bg2 from '../../../Assets/Gallery/Page3/1.svg'
+import { motion } from "framer-motion";
 
 const Page2 = () => {
 
-    return(
+    return (
         <>
-            <div className="px-2 pt-5 w-100 d-flex align-items-end" style={{background:'linear-gradient(#451400,#F58000)',maxHeight:'9000vh'}} >
+            <div className="container-lg position-absolute rangkaian-gallery" style={{zIndex:'10', marginTop: '20vh'}} >
+                <Rangkaian1/>
+            </div>
+
+            <div className="px-2 w-100 d-flex align-items-end" style={{background:'linear-gradient(#451400,#F58000)',minHeight:'110vh'}} >
                 <Bg className="position-absolute" style={{left:'0', zIndex:'1'}}/>
-                <main className="container-lg" style={{zIndex:'10', marginTop:'15vh'}} >
-                    <Rangkaian1/>
-                </main>
+            </div>
+
+            <div className="pt-5 d-flex justify-content-center align-items-end bg-coming" style={{minHeight: '250vh'}}>
+                <section className="position-absolute h-100 w-100 bg-page2-gallery justify-content-center " style={{left:'0',backgroundImage:`url(${Bg2})`,backgroundSize:'cover'}}>
+                    <motion.h2 initial={{opacity:0,y:100}} whileInView={{opacity:1,y:0}} transition={{duration:0.3}} className="page2-gallery text-center " style={{zIndex:'1', marginTop:'20vh'}}>COMING SOON</motion.h2>
+                </section>
             </div>
         </>
     );
---
+++ src/Pages/Gallery/Pages/Page3.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Page1.css';
+import Pl1 from '../../../Assets/Gallery/Page3/1.svg'
+import { motion } from "framer-motion";
+import About from "../../Home/Pages/Components/AboutIfElse";
+import './Page3.css'
+
+const Page3 = () => {
+    return(
+        <>
+            {/* <div className="px-2 pt-5 w-100 d-flex align-items-end justify-content-center bg-coming" style={{height: '230vh', overflow: 'hidden'}}>
+                <img className='position-absolute w-100' style={{zIndex:'0', marginTop: '100vh', paddingBottom: '70vh'}} src={Pl1} alt="pic"></img>
+                <motion.h2 initial={{opacity:0,y:100}} whileInView={{opacity:1,y:0}} transition={{duration:0.3}} className="page3-gallery text-center">COMING SOON</motion.h2>
+            </div> */}
+            <div className="pt-5 d-flex align-items-end justify-content-center bg-coming" style={{minHeight: '160vh'}}>
+                <section className="position-absolute h-100 w-100" style={{left:'0',backgroundImage:`url(${Pl1})`,backgroundSize:'cover',zIndex:0}}/>
+                <motion.h2 initial={{opacity:0,y:100}} whileInView={{opacity:1,y:0}} transition={{duration:0.3}} className="page3-judul text-center">COMING SOON</motion.h2>
+            </div>
+        </>
+    );
+}
+
+export default Page3;
